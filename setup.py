3137113126fdceaa48e184793de12c9c2e165f03
leonardo <leofiore@gmail.com>
2016-03-31 11:50:12 -0400

    prepare for pypi publication

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 from django_imgur import version
 from setuptools import setup
 
+
 def get_packages():
     # setuptools can't do the job :(
     packages = []
@@ -12,14 +13,25 @@ def get_packages():
 
     return packages
 
+
 requires = ['imgurpython']
 
-setup(name='django-imgur',
+
+setup(
+    name='django-imgur',
     version=version,
-    description='A Django App that contains a Django Storage which uses Dropbox.',
-    author=u'Andres Torres Marroquin',
-    author_email='andres.torres.marroquin@gmail.com',
-    url='https://github.com/andres-torres-marroquin/django-dropbox',
+    description='A Django App that contains a Django '
+    'Storage which uses Imgur.'
+    'Inspired, based, and forked from django-dropbox',
+    author=u'leonardo@perpli.me',
+    url='https://github.com/leofiore/django-imgur',
     packages=get_packages(),
     install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
